41d527d0dbb15f744b5ae5941f75f98e042eb94b
rifkiystark <ananda.rifkiy32@gmail.com>
2020-10-10 17:54:50 +0700

    make splashscreen, login, and register page

---
+++ app/src/main/java/com/ngoprek/sosmed/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.ngoprek.sosmed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btnLogin.setOnClickListener {
+            finish()
+        }
+    }
+}
